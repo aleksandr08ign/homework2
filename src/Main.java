e73c797075cf1fbc124a231d258a8db5296c13c8
Александр <ignatenko.ap@yandex.ru>
2023-08-04 23:40:02 +0300

    task1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Задача 1");
+        System.out.println("Задача 1.");
         var dog =  8.0;
         var cat = 3.6;
         var paper = 763789;
@@ -8,7 +8,7 @@ public class Main {
         System.out.println (cat);
         System.out.println (paper);
 
-        System.out.println("Задача 2");
+        System.out.println("Задача 2.");
                 dog = dog + 4;
                 cat = cat +4;
                 paper = paper + 4;
@@ -16,7 +16,7 @@ public class Main {
         System.out.println (cat);
         System.out.println (paper);
 
-        System.out.println("Задача 3");
+        System.out.println("Задача 3.");
         dog = dog - 3.5;
         cat = cat - 1.6;
         paper = paper - 7639;
@@ -24,7 +24,7 @@ public class Main {
         System.out.println (cat);
         System.out.println (paper);
 
-        System.out.println("Задача 4");
+        System.out.println("Задача 4.");
         var friend = 19;
         System.out.println (friend);
         friend = friend + 2;
@@ -32,7 +32,7 @@ public class Main {
         friend = friend / 7;
         System.out.println (friend);
 
-        System.out.println("Задача 5");
+        System.out.println("Задача 5.");
         var frog = 3.5;
         System.out.println (frog);
         frog = frog * 10;
@@ -42,7 +42,7 @@ public class Main {
         frog = frog + 4;
         System.out.println (frog);
 
-        System.out.println("Задача 6");
+        System.out.println("Задача 6.");
         var theFirst = 78.2;
         var theSecond = 82.7;
         System.out.println("Вес первого боксера " + theFirst);
@@ -50,18 +50,18 @@ public class Main {
         System.out.println("Общий вес боксеров " + (theFirst + theSecond));
         System.out.println("Разница в весе " + (theFirst - theSecond));
 
-        System.out.println("Задача 7");
+        System.out.println("Задача 7.");
         System.out.println("Разница в весе (вычитанием) " + (theSecond - theFirst ));
         System.out.println("Разница в весе (остаток от деления) " + (theFirst + theSecond) % theFirst);
 
-        System.out.println("Задача 7");
+        System.out.println("Задача 8.1.");
         var totalHours = 640;
         var oneEmployee = 8;
         System.out.println("Общее кол-во часов - " + totalHours);
         System.out.println("Кол-во часов на одного работника - " + oneEmployee);
         System.out.println("Всего работников в компании - " + (totalHours / oneEmployee) + " человек");
 
-        System.out.println("Задача 8");
+        System.out.println("Задача 8.2.");
         var still = 94;
         System.out.println("Если в компании работает " + (still + totalHours / oneEmployee) + " человек(а), то всего " + ((still + totalHours / oneEmployee) / oneEmployee) + " час(ов) работы может быть поделено между сотрудниками");
 
